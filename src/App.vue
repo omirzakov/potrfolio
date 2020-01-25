b62a29907d7681d59305a534c794ed1a16f84593
huistowns <huistowns@gmail.com>
2020-01-25 13:36:29 +0600

    Upload files

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <p> {{ message }} </p>
+    <button @click="reverseMessage"> Кликни братан!</button>
+
+
+    <input type="text" v-model="message">
+  </div>
+</template>
+
+<script>
+
+export default {
+  el: '#app',
+  data() {
+    return {
+      message: 'Привет , Vue JS'
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      setInterval(() => {
+        this.message = this.message.split('').reverse().join('');
+      }, 300)
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Example.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Hello World!</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Example",
+        props: ['todo']
+    }
+</script>
+
+<style scoped>
+
+</style>
