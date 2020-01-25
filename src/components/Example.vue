0b3c98063d0eec46b667a5dadcbce1758d3c6460
huistowns <huistowns@gmail.com>
2020-01-25 13:40:04 +0600

    Upload files

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -7,7 +7,12 @@
 <script>
     export default {
         name: "Example",
-        props: ['todo']
+        props: ['todo'],
+        data() {
+            return {
+                name: 'Madiyar'
+            }
+        }
     }
 </script>
 
